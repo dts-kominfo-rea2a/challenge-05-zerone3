8d41d5a3c85233d34686f6daae12e4b634cf5041
muhammadniam <zeronejapanniam3@gmail.com>
2022-06-05 22:12:11 +0700

    feat: complete sorter,sortAscending, & sortDescending function

--- index.js
+++ index.js
@@ -6,16 +6,26 @@ const names = ["Halo", "Angel", "Nyoman", "Ketut", "Aisyah"];
 // "1. Nama"
 // "2. Nama"
 // ...
-const sorter = null;
+const sorter = (arrName, fnSort) => {
+  let res = []
+  let resultSort = fnSort(arrName);
+  for (let i = 0; i < resultSort.length; i++) {
+    res.push(`${i+1}. ${arrName[i]}`);
+  }
+  return res;
+};
 
 // TODO: Fungsi untuk mengurutkan array of string secara ascending
 // mengembalikan array yang sudah diurutkan
-const sortAscending = null;
+const sortAscending = (arrName) => {
+  return arrName.sort();
+};
 
 // TODO: Fungsi untuk mengurutkan array of string secara descending
 // mengembalikan array yang sudah diurutkan
-const sortDescending = null;
-
+const sortDescending = (arrName) => {
+  return arrName.sort().reverse();
+};
 // ! JANGAN DIMODIFIKASI
 (function main() {
   console.log(sorter?.(names, sortAscending)?.join("\n"));
